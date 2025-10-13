5bf10678e9f77a15585fc810125160ad441fb0ce
Develop Dorian <developdorian@gmail.com>
2025-10-12 23:10:04 -0500

    feat: config of new routes

---
+++ lib/app/router.dart
@@ -0,0 +1,38 @@
+import 'package:global_app/features/pokedex/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'splash',
+      builder: (context, state) {
+        return const SplashScreen();
+      },
+    ),
+
+    /// Ruta de la lista de Pokemons
+    GoRoute(
+      path: '/pokedex',
+      name: 'pokedex',
+      builder: (context, state) => const PokedexScreen(),
+    ),
+
+    GoRoute(
+      path: '/onboarding',
+      name: 'onboarding',
+      builder: (context, state) => const OnboardingScreen(),
+    ),
+
+    /// Ruta de detalle de un Pokemon
+    GoRoute(
+      path: '/pokedex/:name', // el parámetro dinámico
+      name: 'pokemon-detail',
+      builder: (context, state) {
+        final name = state.pathParameters['name']!;
+        return DetailScreen(name: name);
+      },
+    ),
+  ],
+);
