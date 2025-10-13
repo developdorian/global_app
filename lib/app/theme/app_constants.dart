47c06bfb51e5c2cc3f24edcff9f023885a75c7aa
Develop Dorian <developdorian@gmail.com>
2025-10-12 23:11:54 -0500

    feat: defining app constants

---
+++ lib/app/theme/app_constants.dart
@@ -0,0 +1,39 @@
+class AppConstants {
+
+  // Images
+  static const String pokeballSplashImage = "assets/images/logos/poke_loader.png";
+  static const String magikarpImage = "assets/images/logos/magikarp.png";
+  static const String character1Image = "assets/images/logos/pj_1.png";
+  static const String character2Image = "assets/images/logos/pj_2.png";
+  static const String jigglypuffImage = "assets/images/logos/jigglypuff.png";
+
+  // Logos
+  static const String bugLogo = "assets/images/icons/bug.svg";
+  static const String darkLogo = "assets/images/icons/dark.svg";
+  static const String dragonLogo = "assets/images/icons/Dragon.svg";
+  static const String electricLogo = "assets/images/icons/electric.svg";
+  static const String fairyLogo = "assets/images/icons/fairy.svg";
+  static const String fightingLogo = "assets/images/icons/fighting.svg";
+  static const String fireLogo = "assets/images/icons/fire.svg";
+  static const String flyingLogo = "assets/images/icons/flying.svg";
+  static const String ghostLogo = "assets/images/icons/ghost.svg";
+  static const String grassLogo = "assets/images/icons/grass.svg";
+  static const String groundLogo = "assets/images/icons/ground.svg";
+  static const String iceLogo = "assets/images/icons/ice.svg";
+  static const String normalLogo = "assets/images/icons/normal.svg";
+  static const String poisonLogo = "assets/images/icons/poison.svg";
+  static const String psychicLogo = "assets/images/icons/psychic.svg";
+  static const String rockLogo = "assets/images/icons/rock.svg";
+  static const String steelLogo = "assets/images/icons/steel.svg";
+  static const String waterLogo = "assets/images/icons/water.svg";
+  static const String unknownLogo = "assets/images/icons/unknown.svg";
+
+  static const String heartLogo = "assets/images/icons/Heart.svg";
+  static const String heartSolidLogo = "assets/images/icons/HeartSolid.svg";
+
+  // Colors
+
+
+
+  
+}
