874d6a25e4495f0597736c0cd22f287d07471ea9
Develop Dorian <developdorian@gmail.com>
2025-10-15 18:51:58 -0500

    feat: adding rotation and scale animations to splash screen pokeball

--- lib/features/pokedex/presentation/screens/splash_screen.dart
+++ lib/features/pokedex/presentation/screens/splash_screen.dart
@@ -9,27 +9,83 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _rotationController;
+  late AnimationController _scaleController;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _scaleAnimation;
+
   @override
   void initState() {
     super.initState();
+    
+    _rotationController = AnimationController(
+      duration: const Duration(milliseconds: 1800),
+      vsync: this,
+    );
+    
+    _rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 2 * 3.14159,
+    ).animate(CurvedAnimation(
+      parent: _rotationController,
+      curve: Curves.easeInOut,
+    ));
+    
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+    
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.1,
+    ).animate(CurvedAnimation(
+      parent: _scaleController,
+      curve: Curves.easeInOut,
+    ));
+    
+    _rotationController.repeat();
+    _scaleController.repeat(reverse: true);
+    
     _navigateToOnboarding();
   }
 
   Future<void> _navigateToOnboarding() async {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(milliseconds: 2000));
     if (mounted) {
       context.go('/onboarding');
     }
   }
 
   @override
+  void dispose() {
+    _rotationController.dispose();
+    _scaleController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Image(
-          image: AssetImage(AppConstants.pokeballSplashImage),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: Listenable.merge([_rotationController, _scaleController]),
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: _rotationAnimation.value,
+              child: Transform.scale(
+                scale: _scaleAnimation.value,
+                child: Image.asset(
+                  AppConstants.pokeballSplashImage,
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
