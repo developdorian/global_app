52a96ff91397443876e93f35fa591b44141b83c5
Develop Dorian <developdorian@gmail.com>
2025-10-15 00:16:41 -0500

    feat: region screen complete

--- lib/features/pokedex/presentation/screens/regions_screen.dart
+++ lib/features/pokedex/presentation/screens/regions_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:global_app/app/localization/l10n/app_localizations.dart';
+import 'package:global_app/app/theme/app_constants.dart';
+import 'package:global_app/app/theme/app_theme.dart';
 
 class RegionsScreen extends StatelessWidget {
   const RegionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    final l10n = AppLocalizations.of(context)!;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 40),
+          Image.asset(AppConstants.jigglypuffImage, height: 250),
+          Text(
+            l10n.regionsTitle,
+            textAlign: TextAlign.center,
+            style: AppTheme.textTheme.displaySmall,
+          ),
+          SizedBox(height: 16),
+          Text(
+            l10n.regionsDescription,
+            textAlign: TextAlign.center,
+            style: AppTheme.textTheme.bodyMedium,
+          ),
+        ],
+      ),
     );
   }
 }
