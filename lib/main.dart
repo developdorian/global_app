e618686321c4e9742321ee9c3def87fe4296bec8
Develop Dorian <developdorian@gmail.com>
2025-10-16 15:19:08 -0500

    feat: initialize Hive database with Pokemon model adapters

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:global_app/features/pokedex/data/models/favorite_pokemon_model.dart';
 import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Hive
+  await Hive.initFlutter();
+  
+  // Register Hive adapters
+  if (!Hive.isAdapterRegistered(0)) {
+    Hive.registerAdapter(FavoritePokemonModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(1)) {
+    Hive.registerAdapter(PokemonTypeModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(2)) {
+    Hive.registerAdapter(PokemonAbilityModelAdapter());
+  }
   
   runApp(
     const ProviderScope(
